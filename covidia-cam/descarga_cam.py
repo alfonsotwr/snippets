1d76a8444f198465d47aa65cee1830debca744b6
alfonsotwr <64971658+alfonsotwr@users.noreply.github.com>
2020-09-10 22:45:34 +0200

    líneas que sobran

--- covidia-cam/descarga_cam.py
+++ covidia-cam/descarga_cam.py
@@ -264,9 +264,9 @@ def descargacam():
     if df.index[-1] != df2.index[-1] + dt.timedelta(1):
         raise RuntimeError('Última fecha de las tablas no coincide')
 
-    pd.set_option('display.max_rows', None)
-    print(sr)
-    print(sr.diff())
+    # pd.set_option('display.max_rows', None)
+    # print(sr)
+    # print(sr.diff())
     assert all(sr.diff().dropna() >= 0), 'La serie acumulada no es creciente'
     assert all((sr.index[1:] - sr.index[:-1]).days > 0), 'Fechas no suben'
 
