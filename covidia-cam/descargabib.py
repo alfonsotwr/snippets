362f627e416292e09a42476d1a569d6deb7a039e
alfonsotwr <64971658+alfonsotwr@users.noreply.github.com>
2020-12-04 18:08:43 +0100

    Add files via upload

--- covidia-cam/descargabib.py
+++ covidia-cam/descargabib.py
@@ -11,7 +11,7 @@ ALWAYS_UPDATE = True
 
 
 def descarga(url, fn, isbinary=False, isascii=False, isbackup=False,
-             prevpage=None):
+             prevpage=None, verify=True):
     today = dt.date.today()
 
     ret = True
@@ -31,26 +31,24 @@ def descarga(url, fn, isbinary=False, isascii=False, isbackup=False,
             print('Backup: {} -> {}'.format(fn, backupfn))
             copyfile(fn, backupfn)
 
-
     if not pth.isfile(fn) or changed != today or ALWAYS_UPDATE:
         print('Descargando:', url)
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         with requests.Session() as s:
             if prevpage:
-                s.get(prevpage, headers=headers)
-            r = s.get(url, headers=headers)
+                s.get(prevpage, headers=headers, verify=verify)
+            r = s.get(url, headers=headers, verify=verify)
         if r.status_code == requests.codes.ok:
             if isbinary:
                 with open(fn, 'wb') as fp:
                     fp.write(r.content)
             elif isascii:
-                content = r.content
                 text = r.content.decode('ascii', 'ignore')
                 with open(fn, 'w', encoding='utf-8') as fp:
                     fp.write(text)
             else:
                 with open(fn, 'w', encoding='utf-8') as fp:
-                    fp.write(r.text)
+                    fp.write(r.text.replace('\r\n', '\n'))
         else:
             print('ERROR', r.status_code, 'descargando:', fn)
             ret = False
